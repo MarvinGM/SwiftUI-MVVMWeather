cb149aae486e850561bfc5dc1d65efad96d11c2e
MarvinGM <marvinmahilum091197@gmail.com>
2021-06-28 18:58:26 +0800

    Initial Commit

---
+++ SwiftUI MVVMWeather/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI MVVMWeather
+//
+//  Created by Amben on 6/28/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUI MVVMWeather/SwiftUI_MVVMWeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_MVVMWeatherApp.swift
+//  SwiftUI MVVMWeather
+//
+//  Created by Amben on 6/28/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_MVVMWeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
